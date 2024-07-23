0acc8b9f48be3c285650e0633eb2ec830e4a93ff
siso <116954896+sisology@users.noreply.github.com>
2024-07-23 20:33:36 +0900

    feat: suno api 형식 request 설정

---
+++ src/main/java/tuneandmanner/wiselydiarybackend/music/dto/request/CreateMusicRequest.java
@@ -0,0 +1,26 @@
+package tuneandmanner.wiselydiarybackend.music.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CreateMusicRequest {
+
+    // 프롬프트
+    @NotNull
+    @Size(max = 3000)
+    private final String prompt;
+
+    // 장르
+    private final String tags;
+
+    // default = false
+    private final Boolean customMode = true;
+
+    // 제목
+    private final String title;
+
+}
