dd609d1de85cd87f7d7af742cc28590fa4dc5e28
siso <116954896+sisology@users.noreply.github.com>
2024-07-23 11:48:52 +0900

    chore: 백엔드 기본 설정

---
+++ src/main/java/tuneandmanner/wiselydiarybackend/WiselyDiaryBackendApplication.java
@@ -0,0 +1,13 @@
+package tuneandmanner.wiselydiarybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiselyDiaryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WiselyDiaryBackendApplication.class, args);
+    }
+
+}
