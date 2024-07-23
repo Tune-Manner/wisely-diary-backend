3e1fada075006ea537fc45808dd90c537dbea2f7
siso <116954896+sisology@users.noreply.github.com>
2024-07-23 14:52:51 +0900

    feat: music entity 추가

---
+++ src/main/java/tuneandmanner/wiselydiarybackend/music/domain/entity/Music.java
@@ -0,0 +1,23 @@
+package tuneandmanner.wiselydiarybackend.music.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "music")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Music {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long musicCode;
+    private Long musicPath;
+    private LocalDateTime createdAt;
+}
