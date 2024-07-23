1146ea3160c13b998173ca8f708e6a5abd909d33
siso <116954896+sisology@users.noreply.github.com>
2024-07-23 21:31:14 +0900

    feat: 음악 생성 컨트롤러 작성

---
+++ src/main/java/tuneandmanner/wiselydiarybackend/music/presentation/MusicController.java
@@ -0,0 +1,25 @@
+package tuneandmanner.wiselydiarybackend.music.presentation;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tuneandmanner.wiselydiarybackend.music.domain.entity.Music;
+import tuneandmanner.wiselydiarybackend.music.dto.request.CreateMusicRequest;
+import tuneandmanner.wiselydiarybackend.music.service.MusicService;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/music")
+@RequiredArgsConstructor
+public class MusicController {
+
+    private final MusicService musicService;
+
+    @PostMapping("/create")
+    public Music createSong(@RequestBody CreateMusicRequest request) {
+        return musicService.createSong(request);
+    }
+}
