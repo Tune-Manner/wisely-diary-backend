64894732a72d0260c65b862d65b661066bfa474a
siso <116954896+sisology@users.noreply.github.com>
2024-07-24 16:19:24 +0900

    chore: backend 폴더로 이동

--- backend/src/main/java/tuneandmanner/wiselydiarybackend/music/presentation/MusicController.java
+++ backend/src/main/java/tuneandmanner/wiselydiarybackend/music/presentation/MusicController.java
@@ -7,10 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import tuneandmanner.wiselydiarybackend.music.domain.entity.Music;
 import tuneandmanner.wiselydiarybackend.music.dto.request.CreateMusicRequest;
 import tuneandmanner.wiselydiarybackend.music.service.MusicService;
-import tuneandmanner.wiselydiarybackend.music.service.SunoApiService;
 
 @Slf4j
 @RestController
