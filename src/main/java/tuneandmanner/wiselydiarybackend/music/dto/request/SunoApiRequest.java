e22e0099933ac4ef013b1fa1a067a2cb0f5310d0
siso <116954896+sisology@users.noreply.github.com>
2024-07-24 15:02:53 +0900

    feat: 음악 생성 시 필수 request 설정

--- src/main/java/tuneandmanner/wiselydiarybackend/music/dto/request/SunoApiRequest.java
+++ src/main/java/tuneandmanner/wiselydiarybackend/music/dto/request/SunoApiRequest.java
@@ -1,20 +1,24 @@
 package tuneandmanner.wiselydiarybackend.music.dto.request;
 
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public class CreateMusicRequest {
+public class SunoApiRequest {
 
     // 프롬프트
     @NotNull
-    @Size(max = 3000)
-    private final String prompt;
+    private final String lyrics;
 
     // 장르
     private final String tags;
+
+    // 커스텀 모드
+    private final Boolean customMode;
+
+    // 제목
+    private final String title;
 
 }
