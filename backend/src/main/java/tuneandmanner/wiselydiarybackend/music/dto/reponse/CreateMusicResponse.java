ec7f7f34f32269a474d80d9ee9e53880a5ea1c49
siso <116954896+sisology@users.noreply.github.com>
2024-07-25 13:53:49 +0900

    feat: suno api response 설정

---
+++ backend/src/main/java/tuneandmanner/wiselydiarybackend/music/dto/reponse/CreateMusicResponse.java
@@ -0,0 +1,11 @@
+package tuneandmanner.wiselydiarybackend.music.dto.reponse;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CreateMusicResponse {
+
+    private final String taskId;
+}
