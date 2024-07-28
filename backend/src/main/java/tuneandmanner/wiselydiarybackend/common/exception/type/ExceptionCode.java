8239959f055f853e2fd7bc8d51f1886459923f6a
siso <116954896+sisology@users.noreply.github.com>
2024-07-28 23:29:54 +0900

    feat: exception code 추가

--- backend/src/main/java/tuneandmanner/wiselydiarybackend/common/exception/type/ExceptionCode.java
+++ backend/src/main/java/tuneandmanner/wiselydiarybackend/common/exception/type/ExceptionCode.java
@@ -15,6 +15,9 @@ public enum ExceptionCode {
      * 409: Conflict
      */
 
+    // 400 Error
+    CLIENT_ERROR(400, "클라이언트 에러가 발생했습니다."),
+
     // 401 Error
     UNAUTHORIZED(401, "인증 되지 않은 요청입니다."),
 
@@ -22,9 +25,13 @@ public enum ExceptionCode {
     ACCESS_DENIED(403, "허가 되지 않은 요청입니다."),
 
     // 404 Error
-    NOT_FOUND_TASK_ID(404, "유효한 음악 코드가 아닙니다.");
+    NOT_FOUND_TASK_ID(404, "유효한 음악 코드가 아닙니다."),
+    NOT_FOUND_CLIP_URL(404, "클립 URL을 찾을 수 없습니다."),
 
-    // 409 Error
+    // 500 Error
+    SERVER_ERROR(500, "서버 에러가 발생했습니다."),
+    // Unknown Error
+    UNKNOWN_ERROR(520, "알 수 없는 오류가 발생했습니다.");
 
 
     private final int code;
