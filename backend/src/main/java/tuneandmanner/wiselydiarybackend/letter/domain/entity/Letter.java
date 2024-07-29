9f26e6ba4a9e0c31a61e582e18180eafe4318e5c
siso <116954896+sisology@users.noreply.github.com>
2024-07-29 20:06:19 +0900

    feat: 편지 엔터티 설정

--- backend/src/main/java/tuneandmanner/wiselydiarybackend/letter/domain/entity/Letter.java
+++ backend/src/main/java/tuneandmanner/wiselydiarybackend/letter/domain/entity/Letter.java
@@ -1,4 +1,28 @@
 package tuneandmanner.wiselydiarybackend.letter.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "letter")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class Letter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long letterCode;
+    private String letterContents;
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+    private Long diarySummaryCode;
+
 }
