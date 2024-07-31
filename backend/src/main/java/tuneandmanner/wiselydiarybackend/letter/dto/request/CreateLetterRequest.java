1c8f07c35d8b3621d3bfebf36ac5b722d3d1fb50
siso <116954896+sisology@users.noreply.github.com>
2024-07-31 12:58:38 +0900

    feat: 편지 기능 request, response 설정

--- backend/src/main/java/tuneandmanner/wiselydiarybackend/letter/dto/request/CreateLetterRequest.java
+++ backend/src/main/java/tuneandmanner/wiselydiarybackend/letter/dto/request/CreateLetterRequest.java
@@ -1,4 +1,14 @@
 package tuneandmanner.wiselydiarybackend.letter.dto.request;
 
+import lombok.*;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateLetterRequest {
+
+    private String message;
+    private Long diarySummaryCode;
+
 }
--- backend/src/main/java/tuneandmanner/wiselydiarybackend/letter/dto/response/CreateLetterResponse.java
+++ backend/src/main/java/tuneandmanner/wiselydiarybackend/letter/dto/response/CreateLetterResponse.java
@@ -1,4 +1,27 @@
 package tuneandmanner.wiselydiarybackend.letter.dto.response;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import tuneandmanner.wiselydiarybackend.letter.domain.entity.Letter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@RequiredArgsConstructor
 public class CreateLetterResponse {
+
+    private final Long letterCode;
+    private final Long diarySummaryCode;
+    private final String letterContents;
+    private final LocalDateTime createdAt;
+
+    public static CreateLetterResponse from(Letter letter) {
+
+        return new CreateLetterResponse(
+                letter.getLetterCode(),
+                letter.getDiarySummaryCode(),
+                letter.getLetterContents(),
+                letter.getCreatedAt()
+        );
+    }
 }
