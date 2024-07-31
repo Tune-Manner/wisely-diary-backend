771c3d7482e68bb131e32a2b0caee96771c8273f
siso <116954896+sisology@users.noreply.github.com>
2024-07-31 12:57:40 +0900

    feat: 편지 조회 Exception 설정

--- backend/src/main/java/tuneandmanner/wiselydiarybackend/common/exception/type/ExceptionCode.java
+++ backend/src/main/java/tuneandmanner/wiselydiarybackend/common/exception/type/ExceptionCode.java
@@ -27,6 +27,7 @@ public enum ExceptionCode {
     // 404 Error
     NOT_FOUND_TASK_ID(404, "유효한 음악 코드가 아닙니다."),
     NOT_FOUND_CLIP_URL(404, "클립 URL을 찾을 수 없습니다."),
+    NOT_FOUND_LETTER_CODE(404, "유효한 편지 코드가 아닙니다."),
 
     // 500 Error
     SERVER_ERROR(500, "서버 에러가 발생했습니다."),
