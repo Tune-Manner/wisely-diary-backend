898013cc2996f7a4afba75c360e4edce9ecd2548
jihyeongeun <hyeongeun0924@gmail.com>
2024-08-13 19:56:06 +0900

    feature: 오류해결 및 달력 연동

--- backend/src/main/java/tuneandmanner/wiselydiarybackend/diary/domain/repository/DiaryRepository.java
+++ backend/src/main/java/tuneandmanner/wiselydiarybackend/diary/domain/repository/DiaryRepository.java
@@ -6,9 +6,12 @@ import org.springframework.stereotype.Repository;
 import tuneandmanner.wiselydiarybackend.diary.domain.entity.Diary;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Integer> {
     Optional<Diary> findByMemberIdAndCreatedAtBetween(String memberId, LocalDateTime start, LocalDateTime end);
+
+    List<Diary> findByMemberIdAndCreatedAtBetweenAndDiaryStatus(String memberId, LocalDateTime startOfDay, LocalDateTime endOfDay, String exist);
 }
