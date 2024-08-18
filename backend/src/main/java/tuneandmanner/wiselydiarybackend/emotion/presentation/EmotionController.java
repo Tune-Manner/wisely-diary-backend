a75777ed1ccab2a485ba1f081e2fea51c1e4bfb2
djfksdl <djfksdl@naver.com>
2024-08-19 01:18:23 +0900

    feat : 감정통계 조회 완료!!

--- backend/src/main/java/tuneandmanner/wiselydiarybackend/emotion/presentation/EmotionController.java
+++ backend/src/main/java/tuneandmanner/wiselydiarybackend/emotion/presentation/EmotionController.java
@@ -4,14 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import tuneandmanner.wiselydiarybackend.emotion.domain.entity.Emotion;
 import tuneandmanner.wiselydiarybackend.emotion.dto.request.EmotionRequest;
 import tuneandmanner.wiselydiarybackend.emotion.dto.response.EmotionResponse;
 import tuneandmanner.wiselydiarybackend.emotion.service.EmotionService;
 
-
-import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -27,7 +23,7 @@ public class EmotionController {
         log.info("StatisticsController.monthlyEmotionalStatistics");
 
         EmotionResponse result = statisticsService.selectMonthlyEmotion(request);
-        log.info("여기보기"+ result);
+
         return ResponseEntity.ok(result);
     }
 }
