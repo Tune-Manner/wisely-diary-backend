0c510f86704a32c71909a855e1f387314020760e
universe <hello.its.universe@gmail.com>
2024-08-20 18:37:29 +0900

    fix: AwsProperties 추가 / S3Config, WiselyDiaryBackendApplication 수정

--- backend/src/main/java/tuneandmanner/wiselydiarybackend/WiselyDiaryBackendApplication.java
+++ backend/src/main/java/tuneandmanner/wiselydiarybackend/WiselyDiaryBackendApplication.java
@@ -2,10 +2,14 @@ package tuneandmanner.wiselydiarybackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import tuneandmanner.wiselydiarybackend.common.config.AwsProperties;
+import tuneandmanner.wiselydiarybackend.common.config.SupabaseProperties;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableConfigurationProperties({AwsProperties.class, SupabaseProperties.class})
 public class WiselyDiaryBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(WiselyDiaryBackendApplication.class, args);
