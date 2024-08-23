b4f111d9698b82f583a82d44a6a0f44a78998c02
universe <hello.its.universe@gmail.com>
2024-08-23 16:00:19 +0900

    fix: OpenAIClientConfig, OpenAIClient, OpenAIConfig 수정

--- backend/src/main/java/tuneandmanner/wiselydiarybackend/stt/client/OpenAIClient.java
+++ backend/src/main/java/tuneandmanner/wiselydiarybackend/stt/client/OpenAIClient.java
@@ -1,15 +1,17 @@
 package tuneandmanner.wiselydiarybackend.stt.client;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
+import tuneandmanner.wiselydiarybackend.auth.config.OpenAiConfig;
 import tuneandmanner.wiselydiarybackend.common.config.FeignConfig;
 import tuneandmanner.wiselydiarybackend.stt.dto.WhisperTranscriptionResponse;
 import tuneandmanner.wiselydiarybackend.auth.config.OpenAIClientConfig;
 
-@FeignClient(name = "openai-service", url = "${spring.ai.openai.urls.base-url}", configuration = {OpenAIClientConfig.class, FeignConfig.class})
+@FeignClient(name = "openai-service", url = "#{@openAIClientConfig.getBaseUrl()}", configuration = {OpenAIClientConfig.class, FeignConfig.class})
 public interface OpenAIClient {
-	@PostMapping(value = "${spring.ai.openai.urls.create-transcription-url}", consumes = "multipart/form-data")
+	@PostMapping(value = "#{@openAIClientConfig.getCreateTranscriptionUrl()}", consumes = "multipart/form-data")
 	WhisperTranscriptionResponse createTranscription(@RequestPart("file") MultipartFile file, @RequestPart("model") String model);
 }
