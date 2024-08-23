e6879f25fa541cf7817f88725f45008e95fa65d3
universe <hello.its.universe@gmail.com>
2024-08-23 17:09:59 +0900

    fix: FeignConfig 긴급 수정

--- backend/src/main/java/tuneandmanner/wiselydiarybackend/common/config/FeignConfig.java
+++ backend/src/main/java/tuneandmanner/wiselydiarybackend/common/config/FeignConfig.java
@@ -7,23 +7,22 @@ import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.support.SpringEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class FeignConfig {
-    @Bean
-    public Encoder feignEncoder() {
-        return new SpringFormEncoder(new SpringEncoder(new ObjectFactory<HttpMessageConverters>() {
-            @Override
-            public HttpMessageConverters getObject() {
-                return new HttpMessageConverters(new RestTemplate().getMessageConverters());
-            }
-        }));
+
+    private final ObjectFactory<HttpMessageConverters> messageConverters;
+
+    public FeignConfig(ObjectFactory<HttpMessageConverters> messageConverters) {
+        this.messageConverters = messageConverters;
     }
 
     @Bean
-    public Encoder feignFormEncoder() {
-        return new SpringFormEncoder(new SpringEncoder(() -> new HttpMessageConverters(new RestTemplate().getMessageConverters())));
+    @Primary
+    public Encoder feignEncoder() {
+        return new SpringFormEncoder(new SpringEncoder(messageConverters));
     }
 
     @Bean
